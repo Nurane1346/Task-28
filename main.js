c0f3bdf69d8e2a481fe9d7992d91acef987f7a56
Nurane1346 <nuraneh01@gmail.com>
2024-02-19 00:40:12 +0400

    created file

---
+++ main.js
@@ -0,0 +1,57 @@
+// Task - 1
+
+// let number =[1,2,3,4,5,6,7,8,9,10];
+// let [first,second,...others]=number
+// console.log(first,second,others)
+
+// Task - 2
+
+// let arr = [12,78,45,52,69,48,95,28,42]
+// let number=Math.max(...arr)
+// console.log(number)
+
+// Task-3
+
+// let number=[4,6,8,9,5]
+// let result=number.map(item=>{
+//     return item*2
+// })
+// console.log(number,result)
+
+// // Task-4
+
+// let array = [1, 6, 34, 5, 1, -4, 54, 76, 23, 65, 3, 50, 45, 100, 2346, -52, 6545, 0, 45, 22];
+
+// let result=array.sort((a,b)=>{
+//     return a-b
+// })
+// console.log(result)
+
+// Task -5
+
+// let number = [-1,2,-5,6,7,-3,-9,2]
+
+// let result=number.filter(item=>{
+//   return item<0
+// })
+// console.log(result)
+
+
+// Task -6
+
+// let number = [-1,2,-5,6,7,-3,-9,2,4,8]
+
+// number.forEach(element => {
+//     if(element>0)
+//     console.log(element)
+// });
+
+// Task -7
+
+// const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"];
+
+
+
+
+
+
